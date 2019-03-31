9dbde4c790f5485389db8569447cd453ae0073f2
chengshaohua <tops.cheng@oyohotels.cn>
2019-03-31 22:47:24 +0800

    Observer & Strategy策略模式

---
+++ StandardSubjectImplementation.h
@@ -0,0 +1,17 @@
+//
+//  StandardSubjectImplementation.h
+//  DesignPattern
+//
+//  Created by chengshaohua on 2019/3/31.
+//  Copyright © 2019 oyohotels.cn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface StandardSubjectImplementation : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ StandardSubjectImplementation.m
@@ -0,0 +1,13 @@
+//
+//  StandardSubjectImplementation.m
+//  DesignPattern
+//
+//  Created by chengshaohua on 2019/3/31.
+//  Copyright © 2019 oyohotels.cn. All rights reserved.
+//
+
+#import "StandardSubjectImplementation.h"
+
+@implementation StandardSubjectImplementation
+
+@end
